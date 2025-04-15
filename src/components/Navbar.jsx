82f95da4c2614d3ea5104fef7ce1d81ab2b008e6
Abhishek Choudhary <142403448+AbhishekChoudharyy@users.noreply.github.com>
2025-04-15 18:54:47 +0530

    final2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const NavBar = () => {
       }
     }
   }, [isMenuOpen]);
-  
+
   useEffect(() => {
     let scrollY = 0;
   
@@ -99,7 +99,7 @@ const NavBar = () => {
   return (
     <div
       ref={navContainerRef}
-      className="fixed inset-x-0 top-0 z-[1000] h-16 border-none transition-all duration-700 sm:inset-x-6"
+      className="fixed inset-x-0 top-1 right-1 left-1 z-[1000] h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">
